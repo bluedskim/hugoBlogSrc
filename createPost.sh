ad6e97e3bcc6fcddabbcdca22a3ba9c9c78a3046
Dennis <bluedskim@gmail.com>
2023-11-30 22:52:26 +0900

    자기소개서 수정

--- createPost.sh
+++ createPost.sh
@@ -3,3 +3,4 @@ hugo new './content/posts/tech/CPU와 메모리가 열일하고 있는지 확인
 hugo new './content/posts/tech/유용한 LINUX CLI명령어와 툴들.md'
 hugo new './content/posts/tech/유용한 LINUX 쉡스크립트들.md'
 hugo new './content/posts/tech/PLANTUML 텍스트 기반 다이어그램 드로잉 툴.md'
+hugo new './content/posts/tech/얼굴인식_오픈소스비교-OpenFace, CompreFace.md'
